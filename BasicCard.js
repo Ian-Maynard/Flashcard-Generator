972ab11dea1fbd69d7f693d1f3d006d76e6d8975
Ian-Maynard <maynard.ian@gmail.com>
2017-04-30 14:38:40 -0400

    Basic Cards

--- BasicCard.js
+++ BasicCard.js
@@ -1,19 +1,49 @@
 // Basic Card - Front and Back info only
-
-var errlog = require("fs");   // Error Log file 
-
-
+var cardfile = require("fs");   // Error Log file 
+var bCardPrompt = require("inquirer");
 
 
 var basicCard = function(front, back) {
 	this.front = front;
 	this.back = back;
 
-	this 
-}
+	this.dispBasicCard = function (){
+	console.log("Front "+this.front+" "+this.back);
+	} //display card
+
+	this writeBasicCard = function() {
+	var record = "Front : "+this.front+" - "+"Back : "+this.back;
+	cardfile.addendFile("cardfile.txt",record);	
+	} // Write the Card
+
+} // Basic Card
 
 
+inquirer.bCardPrompt([ 
+{
+    type: "input",
+    name: "front",
+    message: "Enter the question "
+  }, {
+    type: "input",
+    name: "back",
+    message: "Enter the answer "
+  }, {
+        type: "confirm",
+        message: "Add another?",
+        name: "another",
+        default: true
+        }
+	]) then(function(inputs)
+	 		{
+        var card = new basicCard(inmputs.front, inputs.back);
+       	writeBasicCard();
+
+        if(answers.another === true)
+            createCard();
+        else
+            console.log("Exitting add....")
+    });
 
 
-
-// Module.exports 	
+ Module.exports 	
